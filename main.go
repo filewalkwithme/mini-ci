b4e3e6b5967ffc04bc9bd63433347a4c4e02b764
maiconio <maiconscosta@gmail.com>
2015-02-23 01:52:41 -0300

    json structure + check for .mini-ci.yml file

--- main.go
+++ main.go
@@ -1,25 +1,62 @@
 package main
 
 import (
-  "net/http"
-  "io/ioutil"
-  "encoding/json"
-  "bytes"
-  "os"
+        "encoding/json"
+        "fmt"
+        "io/ioutil"
+        "net/http"
 )
 
-func main(){
-  serverCI := http.NewServeMux()
-  serverCI.HandleFunc("/", postReceive)
+type GithubRepository struct {
+        Id       int    `json:"id"`
+        Name     string `json:"name"`
+        FullName string `json:"full_name"`
+}
 
-  http.ListenAndServe(":3000", serverCI)
+type GithubCommit struct {
+        Id        string "id"
+        Message   string `json:"message"`
+        Timestamp string `json:"timestamp"`
+        Url       string `json:"url"`
+}
+
+type GithubPush struct {
+        Ref        string           `json:"ref"`
+        Repository GithubRepository `json:"repository"`
+        HeadCommit GithubCommit     `json:"head_commit"`
+}
+
+func main() {
+        serverCI := http.NewServeMux()
+        serverCI.HandleFunc("/", postReceive)
+
+        http.ListenAndServe(":3000", serverCI)
 }
 
 func postReceive(w http.ResponseWriter, r *http.Request) {
-    x, _ := ioutil.ReadAll(r.Body)
+        x, _ := ioutil.ReadAll(r.Body)
+        defer r.Body.Close()
 
-    var out bytes.Buffer
-    json.Indent(&out, x, "", "\t")
-    out.WriteTo(os.Stdout)
+        var push GithubPush
+        err := json.Unmarshal(x, &push)
+
+        if err == nil {
+                fmt.Printf("%v\n", push)
+                urlYml := "https://api.github.com/repos/" + push.Repository.FullName + "/contents/.mini-ci.yml?ref=" + push.Ref
+                fmt.Printf("GET %v\n", urlYml)
+                resp, err := http.Get(urlYml)
+                if err == nil {
+                        if resp.StatusCode == 200 {
+                                fmt.Printf("File .mini-ci.yml found.Lets build this app!\n")
+                        } else {
+                                fmt.Printf("File .mini-ci.yml not found. Exiting.[%v]\n", resp.Status)
+                        }
+                } else {
+                        fmt.Printf("Err: %v\n", err)
+                }
+        } else {
+                fmt.Printf("Err: %v\n", err)
+        }
 }
+
 
